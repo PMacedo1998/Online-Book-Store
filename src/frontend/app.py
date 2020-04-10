5f59c7f8ef06dc97c0c3214af9bcc7351cd654e3
kimberly-nguyen <43930484+kimberly-nguyen@users.noreply.github.com>
2020-04-10 16:05:26 -0400

    added user's status functionality

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -4,8 +4,8 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 app.config['MYSQL_DATABASE_USER'] = "root"
-app.config['MYSQL_DATABASE_PASSWORD'] = "Clear1998"
-app.config['MYSQL_DATABASE_DB'] = "bookstore"
+app.config['MYSQL_DATABASE_PASSWORD'] = "whatWhat11"
+app.config['MYSQL_DATABASE_DB'] = "csci4050_bookstore"
 app.config['MYSQL__DATABASE_HOST'] = "localhost"
 mysql = MySQL(app)
 
@@ -19,14 +19,14 @@ def register():
         email = request.form['email']
         password = request.form['password']
         phoneNumber = request.form['phoneNumber']
-        shipAdd = "1004"
-        promoApplied = request.form['promoApplied']
-        cursor.execute("INSERT INTO profile(firstName, lastName, email, pswd, phoneNum, shippingAddress, promoApplied) VALUES (%s,%s,%s,%s,%s,%s,%s)", (firstName, lastName, email, password, phoneNumber, shipAdd, promoApplied))
+        cursor.execute("INSERT INTO profile(firstName, lastName, email, pswd, phoneNum) VALUES (%s,%s,%s,%s,%s)", (firstName, lastName, email, password, phoneNumber))
+        status=0
+        verificationCode=""
+        cursor.execute("INSERT INTO registeredUser(status,verificationCode) VALUES (%s,%s)", (status,verificationCode))
         con.commit()
         cursor.close()
         return 'success'
     return render_template('registration.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
@@ -39,5 +39,3 @@ if __name__ == '__main__':
 #age = get.getCredentials("Patrick")
 
 #print(age)
-
-
