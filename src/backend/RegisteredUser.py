7f499b92839d9f5012be09fc9dd3f129001ab32f
sdvickers98@gmail.com <sdvickers98@gmail.com>
2020-04-10 00:15:23 -0400

    implemented UserType.py, Profile.py, started RegisteredUser.py

---
+++ src/backend/RegisteredUser.py
@@ -0,0 +1,45 @@
+import "Profile.py"
+import ShoppingCart
+
+class RegisteredUser():
+	string status
+	int accountID
+	Profile profile
+	ShoppingCart cart
+
+	def logout():
+		pass
+		# TODO implement
+
+	def addToCart(b):
+		pass
+		# TODO implement
+
+	def updatePromoStatus(promoStatus):
+		profile.updatePromoStatus(promoStatus)
+
+	def resetPassword(password):
+		profile.updatePassword(password)
+
+	def checkout():
+		pass
+		# TODO implement
+
+	def returnBook(b):
+		pass
+		# TODO implement
+
+
+	def updateCart()
+		pass 
+		# TODO implement
+
+	def getProfile():
+		return profile
+
+	def getOrderHistory():
+		pass
+		# TODO implement
+
+	def getCart():
+		return cart
