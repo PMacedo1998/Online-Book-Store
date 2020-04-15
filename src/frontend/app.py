9d3bb342e3012c6258be05c08aa75d767b64b854
trosen97 <38743743+trosen97@users.noreply.github.com>
2020-04-15 01:09:29 -0400

    forgot password updated

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -9,7 +9,7 @@ from sendgrid.helpers.mail import *
 app = Flask(__name__)
 
 app.config['MYSQL_DATABASE_USER'] = "root"
-app.config['MYSQL_DATABASE_PASSWORD'] = ""
+app.config['MYSQL_DATABASE_PASSWORD'] = "pw"
 app.config['MYSQL_DATABASE_DB'] = "bookstore"
 app.config['MYSQL__DATABASE_HOST'] = "localhost"
 mysql = MySQL(app)
@@ -28,6 +28,68 @@ def index():
 @app.route('/main', methods = ['GET','POST'])
 def main():
     return render_template('main.html')
+
+#route for forgotten password
+@app.route('/forgotPassword', methods = ['GET', 'POST'])
+def forgotPassword():
+    inputEmail=''
+    if request.method == 'POST':
+
+        inputEmail = request.form['email']
+        cursor.execute('SELECT * FROM profile')
+        users = cursor.fetchall()
+        #generate random code
+        code = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+        for x in users:
+            email = x[4]
+            if inputEmail == email: #account found
+                #generate verification email
+                message = 'You are receiving this email because you requested a password reset. Please enter the following code on the webpage to verify this request: ' + code
+                mail = Mail(from_email = 'tylerrosen97@gmail.com',
+                            to_emails = request.form['email'],
+                            subject = 'Bookstore password reset',
+                            plain_text_content = message
+                            )
+
+                #send verification email
+                sg = sendgrid.SendGridAPIClient(api_key='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')
+                response = sg.client.mail.send.post(request_body=mail.get())
+
+                #store code 
+                cursor.execute ("""
+                                UPDATE profile
+                                SET verificationCode=%s
+                                WHERE email=%s
+                                """, (code, inputEmail))
+                return redirect(url_for('resetPassword'))
+    return render_template("forgotPassword.html")
+    
+
+                
+#route to reset forgotten password
+@app.route('/resetPassword',  methods = ['GET', 'POST'])
+def resetPassword():
+    inputCode=''
+    inputPass=''
+    if request.method == 'POST':
+
+        inputCode = request.form['code']
+        inputPass = request.form['password']
+
+        cursor.execute('SELECT * FROM profile')
+        users = cursor.fetchall()
+        code = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+        for x in users:
+            password = x[5]
+            if inputCode == code: #account found; need to make more secure
+                #store new password
+                cursor.execute ("""
+                                UPDATE profile
+                                SET pswd=%s
+                                WHERE verificationCode=%s
+                                """, (inputPass, code))
+                return render_template("passwordUpdated.html")
+    return render_template("resetPassword.html")
 
 #goes to this after submitting registration info
 @app.route('/account_verification', methods = ['GET','POST'])
@@ -63,6 +125,7 @@ def register():
         email = request.form['email']
         password = request.form['password']
         phoneNumber = request.form['phoneNumber']
+    
         #ensure account with this email does not already exist
         existingEmail = ''
         cursor.execute('SELECT * FROM profile')
@@ -87,7 +150,7 @@ def register():
         #send verification email
         sg = sendgrid.SendGridAPIClient(api_key='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')
         response = sg.client.mail.send.post(request_body=mail.get())
-
+            
         #get payment info
         name = request.form['name']
         cardType = request.form['cardType']
@@ -100,7 +163,7 @@ def register():
         cursor.execute("INSERT INTO paymentMethod(type, cardNumber, expirationDate, name) VALUES (%s,%s,%s,%s)", (cardType, cardNumber, expirationDate, name))
     
         con.commit()
-
+        
         return redirect(url_for('verify'))
     return render_template('registration.html')
 
@@ -123,7 +186,7 @@ def login():
         passWord = x[5]
         if inputEmail == email and inputPass ==passWord:
             isUser = True
-
+            
     if isUser == False:
         return render_template("login.html")
     return redirect(url_for('main'))
@@ -140,3 +203,12 @@ def login():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+#inputName = "Patrick"
+
+#get = getSQLProcessor.getSQLProcessor()
+#age = get.getCredentials("Patrick")
+
+#print(age)
