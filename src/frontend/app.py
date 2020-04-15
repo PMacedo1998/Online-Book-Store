ff8e7d5ca7353c2e5b488aa2e50e500823f907f8
kimberly-nguyen <43930484+kimberly-nguyen@users.noreply.github.com>
2020-04-15 08:14:25 -0400

    fixed html redirection and edit profile function

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -159,10 +159,14 @@ def register():
         cardType = request.form['cardType']
         cardNumber = sha256_crypt.encrypt(request.form['cardNumber'])
         expirationDate = request.form['expirationDate']
-        shippingAddress = request.form['address1'] + request.form['address2'] + request.form['zip'] + request.form['city'] + request.form['state']
+        address1 = request.form['address1']
+        address2 = request.form['address2']
+        zip = request.form['zip']
+        city = request.form['city']
+        state = request.form['state']
 
         #store into dB
-        cursor.execute("INSERT INTO profile(firstName, lastName, phoneNum, email, pswd, shippingAddress, verificationCode) VALUES (%s,%s,%s,%s,%s,%s,%s)", (firstName, lastName, phoneNumber, email, password, shippingAddress, code))
+        cursor.execute("INSERT INTO profile(firstName, lastName, phoneNum, email, pswd, address1, address2, zipcode, city, state, verificationCode) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (firstName, lastName, phoneNumber, email, password, address1, address2, zip, city, state, code))
         cursor.execute("INSERT INTO paymentMethod(type, cardNumber, expirationDate, name) VALUES (%s,%s,%s,%s)", (cardType, cardNumber, expirationDate, name))
 
         con.commit()
@@ -202,6 +206,127 @@ def login():
    #     return render_template('login.html',error = True)
 
     return render_template("login.html")
+
+#display profile information
+@app.route('/editprofile')
+def displayinfo():
+
+    cursor.execute("SELECT firstName FROM profile WHERE id=(SELECT MAX(id) FROM profile);")
+    firstName = cursor.fetchone()
+    if firstName:
+        firstName=firstName[0]
+
+    cursor.execute("SELECT lastName FROM profile WHERE id=(SELECT MAX(id) FROM profile);")
+    lastName=cursor.fetchone()
+    if lastName:
+        lastName=lastName[0]
+
+    cursor.execute("SELECT email FROM profile WHERE id=(SELECT MAX(id) FROM profile);")
+    email=cursor.fetchone()
+    if email:
+        email=email[0]
+
+    cursor.execute("SELECT phoneNum FROM profile WHERE id=(SELECT MAX(id) FROM profile);")
+    phoneNumber=cursor.fetchone()
+    if phoneNumber:
+        phoneNumber=phoneNumber[0]
+
+    cursor.execute("SELECT address1 FROM profile WHERE id=(SELECT MAX(id) FROM profile);")
+    address1=cursor.fetchone()
+    if address1:
+        address1=address1[0]
+
+    cursor.execute("SELECT address2 FROM profile WHERE id=(SELECT MAX(id) FROM profile);")
+    address2=cursor.fetchone()
+    if address2:
+        address2=address2[0]
+
+    cursor.execute("SELECT zipcode FROM profile WHERE id=(SELECT MAX(id) FROM profile);")
+    zipcode=cursor.fetchone()
+    if zipcode:
+        zipcode=zipcode[0]
+
+    cursor.execute("SELECT city FROM profile WHERE id=(SELECT MAX(id) FROM profile);")
+    city=cursor.fetchone()
+    if city:
+        city=city[0]
+
+    cursor.execute("SELECT state FROM profile WHERE id=(SELECT MAX(id) FROM profile);")
+    state=cursor.fetchone()
+    if state:
+        state=state[0]
+
+    cursor.execute("SELECT name FROM paymentMethod WHERE paymentMethodID=(SELECT MAX(paymentMethodID) FROM paymentMethod);")
+    cardName=cursor.fetchone()
+    if cardName:
+        cardName=cardName[0]
+
+
+
+
+
+
+    return render_template('edit_profile.html', fName=firstName,lName=lastName,email=email,phoneNum=phoneNumber,address1=address1,address2=address2,zipcode=zipcode,city=city,state=state,cardName=cardName)
+
+
+#update profile information
+@app.route('/editprofile', methods = ['GET','POST'])
+def updateinfo():
+    if request.method == 'POST':
+
+        #edit personal info
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        password = request.form['password']
+        phoneNumber = request.form['phoneNumber']
+        address1 = request.form['address1']
+        address2 = request.form['address2']
+        zipcode = request.form['zipcode']
+        city = request.form['city']
+        state = request.form['state']
+
+        #update into dB
+        if(firstName!=''):
+            statement="UPDATE profile SET firstName='{}' WHERE id=(MAX(id));".format(firstName)
+            cursor.execute(statement)
+        if(lastName!=''):
+            statement="UPDATE profile SET lastName='{}' WHERE id=23".format(lastName)
+            cursor.execute(statement)
+        if(password!=''):
+            statement="UPDATE profile SET phoneNum='{}' WHERE id=23".format(password)
+            cursor.execute(statement)
+        if(phoneNumber!=''):
+            statement="UPDATE profile SET phoneNum='{}' WHERE id=23".format(phoneNumber)
+            cursor.execute(statement)
+
+        if(address1!=''):
+            statement="UPDATE profile SET address1='{}' WHERE id=23".format(address1)
+            cursor.execute(statement)
+
+        if(address2!=''):
+            statement="UPDATE profile SET address2='{}' WHERE id=23".format(address2)
+            cursor.execute(statement)
+
+        if(zipcode!=''):
+            statement="UPDATE profile SET zipcode='{}' WHERE id=23".format(zipcode)
+            cursor.execute(statement)
+
+        if(city!=''):
+            statement="UPDATE profile SET city='{}' WHERE id=23".format(city)
+            cursor.execute(statement)
+
+        if(state!=''):
+            statement="UPDATE profile SET state='{}' WHERE id=23".format(state)
+            cursor.execute(statement)
+
+
+        con.commit()
+
+        return redirect(url_for('displayinfo'))
+    return render_template('edit_profile.html')
+
+
+
 
 
 
